b249517487b2afc4c6425b3f6949546d52c24dc9
BenguCambay <123668329+BenguCambay@users.noreply.github.com>
2024-08-19 00:45:37 +0300

    Update settings.py

--- BlogDjango/src/cblog/settings.py
+++ BlogDjango/src/cblog/settings.py
@@ -85,7 +85,7 @@ WSGI_APPLICATION = 'cblog.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'blogDjangobc', # database name in RDS is written here
+        'NAME': 'BlogDjangoDB', # database name in RDS is written here
         'USER': 'admin', # database master username in RDS is written here
         'PASSWORD': config('PASSWORD'),
         'HOST': 'blogdjangobc.cb6wwq4okpvf.us-east-1.rds.amazonaws.com',  # database endpoint is written here
@@ -144,7 +144,7 @@ LOGIN_REDIRECT_URL = "blog:list"
 LOGIN_URL = "login"
 
 
-AWS_STORAGE_BUCKET_NAME = 'blogdjango.bc' # please enter your s3 bucket name
+AWS_STORAGE_BUCKET_NAME = 'BlogDjango-RDS-Bucket' # please enter your s3 bucket name
 AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
 AWS_S3_REGION_NAME = "us-east-1" # please enter your s3 region 
 AWS_DEFAULT_ACL = 'public-read'
